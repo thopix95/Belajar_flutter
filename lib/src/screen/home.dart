73ef09fc4fb6cb9a6cf067c6506e9b8c71a528cd
herry88 <herry081288@gmail.com>
2021-07-25 12:32:22 +0700

    deelte widget home

--- lib/src/screen/home.dart
+++ lib/src/screen/home.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
+  //ini homedart
   @override
   Widget build(BuildContext context) {
     return Scaffold(
